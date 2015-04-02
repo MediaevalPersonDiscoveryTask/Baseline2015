216f852d64450fad9e8092d0aecfb1c18ddfae0d
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-02 15:10:08 +0200

    change fct

--- SubComponent/1_Shot/1_extract_descriptor.py
+++ SubComponent/1_Shot/1_extract_descriptor.py
@@ -17,6 +17,7 @@ Options:
 
 from docopt import docopt
 import cv2, cv
+import itertools 
 import numpy as np
 
 def calcul_hist(src_np):  
@@ -32,36 +33,28 @@ def calcul_hist(src_np):
     cv.CalcHist([cv.GetImage(i) for i in [h_plane, s_plane]], hist)
     return hist  
     
-def score_OF(roi, frame1, frame0, lk_params, feature_params):
-    # extract images
-    img0 = cv2.cvtColor(frame0[roi.ymin:roi.ymax, roi.xmin:roi.xmax], cv2.COLOR_BGR2GRAY)
-    img1 = cv2.cvtColor(frame1[roi.ymin:roi.ymax, roi.xmin:roi.xmax], cv2.COLOR_BGR2GRAY)
+def score_OF(img0, img1, lk_params, feature_params):
+    # convert image to gray
+    img0_tmp = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
+    img1_tmp = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
     # find point on the 2 images
-    p0 = cv2.goodFeaturesToTrack(img0, mask = None, **feature_params)    
-    p1 = cv2.goodFeaturesToTrack(img1, mask = None, **feature_params)    
-    if p0 is None or  p1 is None:
-        return -1.0
-    
-    # find point on the other images
-    p1_0, st1_0, err1_0 = cv2.calcOpticalFlowPyrLK(img0, img1, p0, p1, **lk_params)
-    p0_1, st0_1, err0_1 = cv2.calcOpticalFlowPyrLK(img1, img0, p1, p0, **lk_params)
-    
-    # count the number of points find
-    nb_c_p0=0.0
-    nb_p0=0.0
-    nb_c_p1=0.0
-    nb_p1=0.0
-    for pts0, pts1, s in itertools.izip(p0, p1_0, st1_0):
-        nb_p0+=1
-        if s[0] == 1:
-            nb_c_p0+=1
-    for pts0, pts1, s in itertools.izip(p1, p0_1, st0_1):
-        nb_p1+=1
-        if s[0] == 1:
-            nb_c_p1+=1
+    p0 = cv2.goodFeaturesToTrack(img0_tmp, **feature_params)    
+    p1 = cv2.goodFeaturesToTrack(img1_tmp, **feature_params)    
+    if p0 is None and p1 is None:
+        return 1.0
 
-    return (nb_c_p0/nb_p0+nb_c_p1/nb_p1)/2
+    # find point on the other images    
+    st1_0, st0_1 = [], []
+    if p0 is not None:
+        p0_1, st0_1, err0_1 = cv2.calcOpticalFlowPyrLK(img0_tmp, img1_tmp, p0, None, **lk_params)
+        st0_1 = list(np.array(st0_1).T[0])
+    if p1 is not None:
+        p1_0, st1_0, err1_0 = cv2.calcOpticalFlowPyrLK(img1_tmp, img0_tmp, p1, None, **lk_params)
+        st1_0 = list(np.array(st1_0).T[0])
+
+    # return proportion of point retrieved
+    return np.mean(st0_1 + st1_0)
 
 
 if __name__ == '__main__': 
