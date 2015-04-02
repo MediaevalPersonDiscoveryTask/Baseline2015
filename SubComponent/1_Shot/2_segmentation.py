53ec3d99477834094c521bfee859258fa35d2716
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-02 17:34:56 +0200

    pic inversé

--- SubComponent/1_Shot/2_segmentation.py
+++ SubComponent/1_Shot/2_segmentation.py
@@ -7,13 +7,13 @@ Usage:
   segmentation.py -h | --help
 Options:
   --min_duration=<md>  minimum duration (in frame) of a shot (>0) [default: 15]  
-  --threshold=<t>      threshold value  (0.0 > t > 1.0), default = mean(score) + X_sigma * std(score)
-  --X_sigma=<Xs>       threshold = mean(score) + X_sigma * std(score) [default: 1.0]
+  --threshold=<t>      threshold value  (0.0 > t > 1.0), default = mean(score) - X_sigma * std(score)
+  --X_sigma=<Xs>       threshold = mean(score) - X_sigma * std(score) [default: 1.0]
   --mix=<m>            mix between the descriptor (mean or product) [default: mean]
 """
 
 from docopt import docopt
-from scipy.signal import argrelmax
+from scipy.signal import argrelmin
 import numpy as np
 from itertools import izip, islice
 
@@ -26,18 +26,12 @@ if __name__ == '__main__':
     y = []
     for line in open(args['<descriptor>']).read().splitlines():
         frame, hist, OF = line.split(' ')
-        hist = float(hist)
-        if hist < 0.0:
-            hist = 0.0
-        hist = 1.0-hist
         OF = float(OF)
-        OF = 1.0-OF
-
+        hist = max(0.0, float(hist))
         if args['--mix'] == 'mean':
-            score = (float(hist)+float(OF))/2
+            score = (hist+OF)/2
         elif args['--mix'] == 'product':
-            score = float(hist)*float(OF)
-
+            score = hist*OF
         x.append(score)
         y.append(int(frame))
 
@@ -46,18 +40,18 @@ if __name__ == '__main__':
     if threshold:
         float(threshold)
     else:
-        threshold = np.mean(x) + float(args['--X_sigma'])*np.std(x)
+        threshold = np.mean(x) - float(args['--X_sigma'])*np.std(x)
     print 'threshold used', threshold
 
     # find local minima
-    minima = argrelmax(np.array(x), order=int(args['--min_duration']))
+    minima = argrelmin(np.array(x), order=int(args['--min_duration']))
 
     # select frame in minima with a score lower than the threshold
     # and save segmentation
     fout = open(args['<output_file>'], 'w')
     fout.write('0\n')
     for i in list(minima)[0]:
-        if x[i] > threshold:
+        if x[i] < threshold:
             fout.write(str(y[i])+'\n')
     fout.write(str(y[-1]))
 
