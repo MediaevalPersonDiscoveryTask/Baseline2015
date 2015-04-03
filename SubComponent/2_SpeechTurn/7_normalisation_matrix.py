a98349b0a4c89dd91cd50f21a3f7b9d52748c5c2
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-04 00:08:23 +0200

    initial add - to be tested

---
+++ SubComponent/2_SpeechTurn/7_normalisation_matrix.py
@@ -0,0 +1,33 @@
+"""
+Compute speaker versus speaker distance
+
+Usage:
+  compute_svs_matrix_perceptron_model.py <videoId> <segmentation_path> <matrix_path> <model_file> <output_path>
+  compute_svs_matrix_perceptron_model.py -h | --help
+"""
+
+from docopt import docopt
+from sklearn.externals import joblib
+import numpy as np
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    # open model
+    clas = joblib.load(arguments['<model_file>']) 
+
+    # read segmentation
+    seg_st = MDTMParser().read(args['<segmentation_path>']+'/'+arguments['<videoId>']+'.mdtm')(uri=arguments['<videoId>'], modality="speaker")
+
+    dur = {}
+    for seg in seg_st.get_timeline():
+        st = list(seg_st.get_labels(seg))[0] 
+        dur[st] = seg.duration
+
+    # compute score between speech turn and save it
+    fout = open(arguments['<output_path>']+'/'+videoId+'.svs', 'w')
+    for line in open(args['<matrix_path>']+'/'+arguments['<videoId>']+'.mat').read().splitlines():
+        st1, st2, BIC_dist = line.split(' ')
+        desc = [float(BIC_dist), min(dur(st1), dur(st2)), max(dur(st1), dur(st2))]
+        fout.write(st1+' '+st2+' '+str(clas.decision_function([desc])[0][1])+'\n')
+    fout.close()
