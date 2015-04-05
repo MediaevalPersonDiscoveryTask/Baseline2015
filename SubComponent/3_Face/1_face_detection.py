c6d724c1cb03e476d039c73261f7b2a750a20813
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-05 22:32:21 +0200

    ...

--- SubComponent/3_Face/1_face_detection.py
+++ SubComponent/3_Face/1_face_detection.py
@@ -2,138 +2,42 @@
 Face detection based on Viola & Jones method
 
 Usage:
-  face_detection.py <video> <output_path> <cascade_file> [--scaleFactor=<sf>] [--minNeighbors=<mn>] [--minSize=<min>] [--startframe=<sf>] [--endframe=<ef>] [--detectionEveryXFrame=<sk> | --pathFileListFrame=<pflf>]
+  face_detection.py <video_file> <output_file> <cascade_file> [--scaleFactor=<sf>] [--minNeighbors=<mn>] [--minSize=<min>]
   face_detection.py -h | --help
 Options:
-  --scaleFactor=<sf>            scaleFactor value [default: 1.1]
-  --minNeighbors=<mn>           minNeighbors value [default: 4]
-  --minSize=<min>               min size of a face [default: 50]
-  --startframe=<ef>             first frame to process [default: 0]
-  --endframe=<ef>               last frame to process default last frame
-  --detectionEveryXFrame=<sk>   nb frame to skip after each face detection [default: 1]
-  --pathFileListFrame=<pflf>    path that contain the file with the list of frame to process
-
+  --scaleFactor=<sf>    scaleFactor value (>1.0) [default: 1.1]
+  --minNeighbors=<mn>   minNeighbors value (>1) [default: 4]
+  --minSize=<min>       min size of a face (>1) [default: 50]
 """
 
-import numpy as np
-import cv2, cv, sys, os
-import json
 from docopt import docopt
-
-import tools
+import cv2, cv
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    video = arguments['<video>']
-    if not os.path.isfile(video):
-        print "error: can't find the video:", video
-        sys.exit()
-    else:
-        print 'video:', video
-
-    path_out = arguments['<output_path>']
-    if not os.path.isdir(path_out):
-        print "error: can't find folder:", path_out
-        sys.exit() 
-    else:
-        print 'path_out:', path_out
-
-    cascade_file = arguments['<cascade_file>']
-    if not os.path.isfile(cascade_file):
-        print "error: can't find the cascade:", cascade_file
-        sys.exit()
-    else:
-        print 'cascade_file:', cascade_file
-
-    scaleFactor = arguments['--scaleFactor']
-    scaleFactor = float(scaleFactor)
-    if scaleFactor <= 1.0 :
-        print 'error: scaleFactor must be higher than 1.0'
-        sys.exit()
-    else:
-        print 'scaleFactor:', scaleFactor
-
-    minNeighbors = arguments['--minNeighbors']
-    minNeighbors = int(minNeighbors)
-    if minNeighbors < 1 :
-        print 'error: minNeighbors must be higher than 1'
-        sys.exit()  
-    else:
-        print 'minNeighbors:', minNeighbors
-
-    minSize = arguments['--minSize']
-    minSize = (int(minSize), int(minSize))
-    if minSize < 1 :
-        print 'error: minSize must be higher than 1'
-        sys.exit()  
-    else:
-        print 'minSize: ', minSize
-
-    pathFileListFrame = arguments['--pathFileListFrame']
-    if pathFileListFrame:
-        if not os.path.isfile(pathFileListFrame+'/'+video.split('/')[-1]+'.frame'):
-            print "error: can't find the FileListFrame:", pathFileListFrame+'/'+video.split('/')[-1]+'.frame'
-            sys.exit()  
-        else:
-            print 'FileListFrame:', pathFileListFrame+'/'+video.split('/')[-1]+'.frame'
-    l_frame = []
-    if pathFileListFrame:
-        for line in open(pathFileListFrame+'/'+video.split('/')[-1]+'.frame'):
-            l_frame.append(int(line[:-1]))
-        print 'proceed only', len(l_frame), 'frames'
-
-    detectionEveryXFrame = arguments['--detectionEveryXFrame']
-    detectionEveryXFrame = int(detectionEveryXFrame)    
-    if detectionEveryXFrame < 1:
-        print 'error: detectionEveryXFrame must be equal or higher than 1'
-        sys.exit()
-    else:
-        print 'proceed face detection every', detectionEveryXFrame, 'frames'
-
-
-    capture = cv.CaptureFromFile(video)
-
-    startframe = arguments['--startframe']
-    endframe = arguments['--endframe']
-    startframe = int(startframe)
-    if endframe:
-        endframe = int(endframe)
-    else:
-        endframe = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT)-1)
-
-    if startframe > endframe :
-        print 'error: startframe must be higher than endframe'
-        sys.exit()
-    if startframe < -1 or endframe < -1:
-        print 'error: startframe and endframe must be higher than 0'
-        sys.exit()
-
-    print "proceed frame from", startframe, 'to', endframe
-
-
+    # read args
+    args = docopt(__doc__)
+    # storage for face detected  
     storage = cv.CreateMemStorage()
-    cascade = cv.Load(cascade_file)
+    # load cascade file
+    cascade = cv.Load(args['<cascade_file>'])
+    # read video
+    capture = cv.CaptureFromFile(args['<video_file>'])
+    nb_frame = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT)-1)
+    frame = cv.QueryFrame(capture)
+    c_frame = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES))
 
-    c_frame = 0    
-    data_out = {}
-    while True:
-        image = cv.QueryFrame(capture)
-        c_frame = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES))
-        if c_frame >= startframe:
-            process_frame = False
-            if (l_frame == [] and c_frame >= startframe and c_frame%detectionEveryXFrame == 0):
-                process_frame = True
-            elif c_frame in l_frame: 
-                process_frame = True
-
-            if process_frame: 
-                data_out[c_frame] = [] 
-                detected = cv.HaarDetectObjects(image, cascade, storage, scaleFactor, minNeighbors, cv.CV_HAAR_DO_CANNY_PRUNING, minSize)
-                for (x,y,w,h),n in detected:  
-                    data_out[c_frame].append({'x':str(x), 'y':str(y), 'w':str(w), 'h':str(h), 'n':str(n)})
-
-        if c_frame > endframe:
-            break
-
-    with open(path_out+'/'+video.split('/')[-1]+'.face.json', 'w') as f:
-        json.dump(data_out, f, indent=4, sort_keys=True) 
+    # save face detection
+    fout = open(args['<output_file>'], 'w')
+    while (c_frame<nb_frame):
+        frame = cv.QueryFrame(capture)
+        detected = cv.HaarDetectObjects(frame, 
+                                        cascade, 
+                                        storage, 
+                                        float(args['--scaleFactor']), 
+                                        int(args['--minNeighbors']), 
+                                        cv.CV_HAAR_DO_CANNY_PRUNING, 
+                                        (int(args['--minSize']), int(args['--minSize'])))
+        for (x,y,w,h),n in detected:  
+            fout.write(str(int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES))))
+            fout.write(' '+str(x)+' '+str(y)+' '+str(w)+' '+str(h)+' '+str(n)+'\n')
+    fout.close()
--- SubComponent/3_Face/2_face_tracking.py
+++ SubComponent/3_Face/2_face_tracking.py
@@ -2,21 +2,18 @@
 Face tracking based on the face detection and optical flow for miss detection
 
 Usage:
-  face_tracker.py <video> <output_path> <cut_path> <detected_face_path> [--threshold_OF=<of>] [--threshold_cov=<tc>]
+  face_tracker.py <video_file> <shot_seg_file> <detection_face_file> <output_file> [--threshold_OF=<of>] [--threshold_cov=<tc>]
   face_tracker.py -h | --help
 Options:
   --threshold_OF=<of>       value of the threshold on the optical flow for the tracking [default: 0.3]
   --threshold_cov=<tc>      if the coverage of 2 boxes is higher than threshold_cov, we consider they correspond to the same face track [default: 0.2]
 """
 
-
+from docopt import docopt
+import cv2, cv
 import numpy as np
 import math
-import cv2, cv
-import json, glob, os
-from docopt import docopt
-
-import tools
+from mediaeval_util.imageTools import OpticalFlow, score_coverage_ROI
 
 lk_params = dict( winSize  = (20, 20), 
                   maxLevel = 2, 
@@ -27,7 +24,7 @@ feature_params = dict( maxCorners = 100,
                        minDistance = 10,
                        blockSize = 3)
 
-def find_new_face(detected_face, face_to_cluster, nb_face_cluster, l_frames_shot, backward, threshold_OF, threshold_cov):
+def find_new_face(l_faces, face_to_cluster, nb_face_cluster, l_frames_shot, backward, threshold_OF, threshold_cov):
     step = 1
     if backward:                                                # if the tracking is proceed backward
         step = -1
@@ -37,23 +34,27 @@ def find_new_face(detected_face, face_to_cluster, nb_face_cluster, l_frames_shot
         new_face_find_by_tracking[f_nb] = {}
 
     for f_nb in sorted(l_frames_shot, reverse=backward):
-        for i_face, face in detected_face[f_nb].items(): # for face find by the detection
-            roi_current = face['roi']
+        for i_face, face in l_faces[f_nb].items(): # for face find by the detection
+            xmin, ymin, xmax, ymax = face['x'], face['y'], face['x']+face['w'], face['y']+face['h']
             l_temp = {}
             nbFrameTracking = nb_face_cluster[face_to_cluster[i_face]] + face['sqrt_Neighbors']
             for i in range(1, nbFrameTracking):
                 if f_nb+i*step in l_frames_shot and f_nb+i*step-step in l_frames_shot:                # check if the next frame is in the shot     
-                    s, nb_p0, nb_p1, move_x, move_y, l_move_x, l_move_y = tools.score_OF(roi_current, l_frames_shot[f_nb+i*step-step], l_frames_shot[f_nb+i*step], lk_params, feature_params)
-                    roi_current = tools.ROI(int(roi_current.xmin+move_x), int(roi_current.ymin+move_y), int(roi_current.xmax+move_x), int(roi_current.ymax+move_y)) 
+                    s, l_move_x, l_move_y = OpticalFlow(l_frames_shot[f_nb+i*step-step][ymin:ymax, xmin:xmax], l_frames_shot[f_nb+i*step][ymin:ymax, xmin:xmax], lk_params, feature_params)
+                    xmin = xmin+np.mean(l_move_x)]
+                    xmax = xmax+np.mean(l_move_x)]
+                    ymin = ymin+np.mean(l_move_y)]
+                    ymax = ymax+np.mean(l_move_y)]
 
                     if s < threshold_OF:                    
                         break
                     else:
                         best_coverage = 0.0
                         best_i_face = ''
-                        for i_face2, face_next in detected_face[f_nb+i*step].items():  
-                            roi_next = face_next['roi']
-                            coverage = tools.score_coverage_ROI(roi_current, roi_next)  # compute the coverage between the 2 roi
+                        for i_face2, face_next in l_faces[f_nb+i*step].items():  
+                            xmin_next, ymin_next, xmax_next, ymax_next = face_next['x'], face_next['y'], face_next['x']+face_next['w'], face_next['y']+face_next['h']
+
+                            coverage = score_coverage_ROI([xmin, ymin, xmax, ymax], [xmin_next, ymin_next, xmax_next, ymax_next])  # compute the coverage between the 2 roi
                             if coverage > best_coverage:            
                                 best_coverage = coverage  
                                 best_i_face = i_face2 
@@ -64,89 +65,71 @@ def find_new_face(detected_face, face_to_cluster, nb_face_cluster, l_frames_shot
                             for f_nb2, roi in sorted(l_temp.items()):   # add the box to the list of face tracked
                                 new_face_find_by_tracking[f_nb2][i_face] = roi
                             break
-                        l_temp[f_nb+i*step] = roi_current           # Add face  to the temporary list
+                        l_temp[f_nb+i*step] = [xmin, ymin, xmax, ymax]           # Add face  to the temporary list
+
                 else:
                     nb_face_cluster[face_to_cluster[i_face]]+=1
                     for f_nb2, roi in sorted(l_temp.items()):   # add the box to the list of face tracked
                         new_face_find_by_tracking[f_nb2][i_face] = roi                    
 
-    for f_nb in sorted(l_frames_shot):                          # copy the face find by tracking into detected_face
+    for f_nb in sorted(l_frames_shot):                          # copy the face find by tracking into l_faces
         for i_face, roi in new_face_find_by_tracking[f_nb].items(): 
-            detected_face[f_nb][i_face] = {'roi':roi, 'sqrt_Neighbors':0, 'Neighbors':0}
+            l_faces[f_nb][i_face] = {'roi':roi, 'sqrt_Neighbors':0, 'Neighbors':0}
 
-    return detected_face, face_to_cluster, nb_face_cluster
+    return l_faces, face_to_cluster, nb_face_cluster
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    video = arguments['<video>']
-    path_out = arguments['<output_path>']
-    cut_path = arguments['<cut_path>']
-    detected_face_path = arguments['<detected_face_path>']
-    threshold_OF = arguments['--threshold_OF']
-    threshold_OF = float(threshold_OF)
-    threshold_cov = arguments['--threshold_cov']
-    threshold_cov = float(threshold_cov)
-
-    l_cut = []                                                  # list of shot boundaries
-    for line in open(cut_path+'/'+video.split('/')[-1]+'.cut'):
-        l_cut.append(int(line[:-1]))
- 
-    capture = cv2.VideoCapture(video)                           # read the video
+    # read arguments
+    args = docopt(__doc__)
+    # read file with the list of shot
+    shot_boundaries = []                                                  # list of shot boundaries
+    for line in open(args['<shot_seg_file>']).read().splitlines():
+        videoId, shot, startTime, endTime, startFrame, endFrame = line.split(' ') 
+        shot_boundaries.append(int(endFrame))
+    # open the video
+    capture = cv2.VideoCapture(args['<video_file>'])            # read the video
     nb_frame = int(capture.get(cv.CV_CAP_PROP_FRAME_COUNT)-1)   # total number of frame in the video
-    print 'nb_frame', nb_frame
-
-    detected_face = {}                                          # face detected
+    # initialize the dictionnary with the list of face detected
+    l_faces = {}                                                # face detected
     for i in range(1, nb_frame+2):
-        detected_face[i] = {}
+        l_faces[i] = {}
+    # read face detection
+    face_to_cluster = {}                                        # name of the face cluster 
+    nb_face_cluster = {}                                        # number of face of a face track
+    nb_face = 0
+    for line in open(args['<detection_face_file>']).read().splitlines():
+        c_frame, x, y, w, h, n = line.split(' ')
+        nb_face+=1
+        face_to_cluster[nb_face] = nb_face
+        nb_face_cluster[nb_face] = 1
+        l_faces[int(c_frame)][nb_face] = {'Neighbors':int(n), 
+                                          'sqrt_Neighbors':int(math.sqrt(int(n)))), 
+                                          'x':int(x), 
+                                          'y':int(y), 
+                                          'w':int(w), 
+                                          'h':int(h)}
 
-    dic_face_detection = json.load(open(detected_face_path+'/'+video.split('/')[-1]+'.face.json'))
-    face_to_cluster = {}                                       # name of the face cluster 
-    nb_face_cluster = {}
-    nb_face = 1
-
-    for frame in sorted(map(float, dic_face_detection.keys())):
-        for face in dic_face_detection[str(int(frame))]:
-            detected_face[int(round(frame,0))][nb_face] = {'Neighbors':int(face['n']), 'sqrt_Neighbors':int(math.sqrt(int(face['n']))), 'roi':tools.ROI(int(face['x']), int(face['y']), int(face['w'])+int(face['x']), int(face['h'])+int(face['y']))}
-            face_to_cluster[nb_face] = nb_face
-            nb_face_cluster[nb_face] = 1
-            nb_face+=1                                          # name of the face detected
-    
-    c_frame = 0                                                 # number of the current frame read in the video
+    ret, frame = capture.read()
+    c_frame = capture.get(cv.CV_CAP_PROP_POS_FRAMES)
     data_out = {}
-    l_frames_shot = {}                                          # list of frame number in the current shot 
+    l_frames_shot = {}                                          # frames of the current shot 
+    fout = open(args['<output_file>'], 'w')
     while (c_frame<nb_frame):
-        ret, frame = capture.read()                             # read the video
-        c_frame = capture.get(cv.CV_CAP_PROP_POS_FRAMES)  
-        if frame.any() :                                        # if there an image in frame
-            if c_frame in l_cut:                                # if the frame is a shot boundaries, proceed the tracking
-                print c_frame
+        ret, frame = capture.read()                             # get the next image
+        c_frame = int(capture.get(cv.CV_CAP_PROP_POS_FRAMES))  
+        if ret :                                                # if there is an image in the frame
+            if c_frame in shot_boundaries:                      # if the frame is a shot boundaries, proceed the tracking
                 # tracking forward
-                detected_face, face_to_cluster, nb_face_cluster = find_new_face(detected_face, face_to_cluster, nb_face_cluster, l_frames_shot, False, threshold_OF, threshold_cov)
+                l_faces, face_to_cluster, nb_face_cluster = find_new_face(l_faces, face_to_cluster, nb_face_cluster, l_frames_shot, False, float(args['--threshold_OF']), float(args['--threshold_cov']))
                 # tracking backward
-                detected_face, face_to_cluster, nb_face_cluster = find_new_face(detected_face, face_to_cluster, nb_face_cluster, l_frames_shot, True, threshold_OF, threshold_cov)
+                l_faces, face_to_cluster, nb_face_cluster = find_new_face(l_faces, face_to_cluster, nb_face_cluster, l_frames_shot, True, float(args['--threshold_OF']), float(args['--threshold_cov']))
                 for f_nb in sorted(l_frames_shot):              # for frame in the current shot
-                    data_out[f_nb] = {}
-                    for i_face, face in detected_face[f_nb].items():
-                        data_out[f_nb][face_to_cluster[i_face]] = {'Neighbors':face['Neighbors'], 'x':face['roi'].xmin, 'y':face['roi'].ymin, 'w':face['roi'].xmax-face['roi'].xmin, 'h':face['roi'].ymax-face['roi'].ymin}
-                #l_frames_shot = {}                              # clear the list of frame for the next shot
+                    fout.write(str(f_nb)+' '+str(len(l_faces[f_nb])))
+                    for i_face, face in l_faces[f_nb].items():
+                        fout.write(' '+str(i_face)+' '+str(face['x'])+' '+str(face['y'])+' '+str(face['w'])+' '+str(face['h']))
+                    fout.write('\n')
                 l_frames_shot.clear()
             l_frames_shot[c_frame] = frame.copy()               # copy the current frame
-
     capture.release()                                           # relaese the video
-
-    facetrack = {}
-    for c_frame in data_out:
-        i_frame = int(round(float(c_frame),0))
-        facetrack[i_frame] = {}
-        for i_face, face in data_out[c_frame].items():
-            facetrack[i_frame][int(i_face)] = face
-
-    fout = open(path_out+'/'+video.split('/')[-1]+'.avi.facetrack', 'w')
-    for c_frame in sorted(facetrack):
-        fout.write(str(c_frame)+' '+str(len(facetrack[c_frame])))
-        for i_face, face in sorted(facetrack[c_frame].items()):
-            fout.write(' '+str(i_face)+' '+str(face['x'])+' '+str(face['y'])+' '+str(face['w'])+' '+str(face['h']))
-        fout.write('\n')
-    fout.close()
 
