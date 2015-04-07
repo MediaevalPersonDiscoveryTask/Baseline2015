d441e8f40444937c73515496801a2494a323bd9b
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-07 12:10:42 +0200

    write more information between racetrack

--- SubComponent/3_Face/5_compute_hvh_matrix.py
+++ SubComponent/3_Face/5_compute_hvh_matrix.py
@@ -24,14 +24,18 @@ if __name__ == '__main__':
     dic = {}
     for line in open(args['<facetracks_descriptor>']).read().splitlines():
         l = line.split(' ')
-        dic[int(l[0])] = np.array(l[2:], dtype='|S20').astype(np.float)
+        dic[int(l[0])] = [int(l[1]), float(l[2]), np.array(l[3:], dtype='|S20').astype(np.float)]
 
     # compute and save distance between face tracks
     fout = open(args['<matrix_output>'], 'w')
     for h1 in sorted(dic):
         for h2 in sorted(dic):
             if h1 < h2:
-                fout.write(str(h1)+' '+str(h2)+' '+str(round(sqdist(dic[h1], dic[h2]), 2))+'\n')
+                fout.write(str(h1)+' '+str(h2))
+                fout.write(' '+str(dic[h1][0])+' '+str(dic[h2][0]))             # number of HoG extracted
+                fout.write(' '+str(dic[h1][1])+' '+str(dic[h2][1]))             # mean distance of the central desccriptor to other descriptors
+                fout.write(' '+str(round(sqdist(dic[h1][2], dic[h2][2]), 2)))   # l2 distance between descriptors
+                fout.write('\n')
     fout.close()
                     
                 
