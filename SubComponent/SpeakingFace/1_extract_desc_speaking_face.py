0c60e95c1e9ab73d453af894c7e32cbd3a01cd19
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-27 14:51:57 +0200

    desc per frame

--- SubComponent/SpeakingFace/1_extract_desc_speaking_face.py
+++ SubComponent/SpeakingFace/1_extract_desc_speaking_face.py
@@ -2,7 +2,7 @@
 Extract descriptor to find speaking face
 
 Usage:
-  extract_desc_svh.py <video> <idx_file> <flandmark> <st_seg> <output_desc>
+  extract_desc_svh.py <video> <flandmark> <output_desc>
   extract_desc_svh.py -h | --help
 """
 
@@ -62,10 +62,15 @@ def read_flandmark_file(f_flandmark):
             flandmark[frame][head] = [x, y, w, h, mx1, my1, mx2, my2]
     return flandmark
 
-def compute_features(flandmark, video_file, frame2time, st_seg):
-    desc = {}
+if __name__ == "__main__": 
+    # read arguments   
+    args = docopt(__doc__)
+    fout = open(args['<output_desc>'], 'w')
 
-    capture = cv2.VideoCapture(video_file)
+    flandmark = read_flandmark_file(args['<flandmark>'])
+
+    capture = cv2.VideoCapture(args['<video>'])
+
     ret, frame_current = capture.read()
     frame_previous1 = frame_current.copy()
     video_w = capture.get(cv.CV_CAP_PROP_FRAME_WIDTH) 
@@ -78,160 +83,104 @@ def compute_features(flandmark, video_file, frame2time, st_seg):
         frame_previous1 = frame_current.copy()
         ret, frame_current = capture.read()
         frameID = int(capture.get(cv.CV_CAP_PROP_POS_FRAMES))
-        timestamp = frame2time(frameID, capture.get(cv.CV_CAP_PROP_POS_MSEC)/1000.0)
 
-        if frame_current.any() and frameID in flandmark and frameID-1 in flandmark and frameID-2 in flandmark : # and  frameID > 7050 and frameID < 7070 :
+        if frame_current.any() and frameID in flandmark and frameID-1 in flandmark and frameID-2 in flandmark :
+            nb_face = len(flandmark[frameID])
+            l_size = []
+            for head, p in flandmark[frameID].items():
+                x, y, w, h, mx1, my1, mx2, my2 = p
+                l_size.append(w*h)
 
-            current_st = ''
-            for startTimeSt, endTimeSt, st in st_seg:
-                if timestamp >= startTimeSt and timestamp <= endTimeSt:
-                    current_st = st
-                    break
+            for head, p in flandmark[frameID].items():
+                x, y, w, h, mx1, my1, mx2, my2 = p
+                fout.write(str(frameID)+" "+str(head))
 
-            if current_st != '':
-                desc.setdefault(current_st, {})
-
-                l_size = []
-                nb_face = len(flandmark[frameID])
-                for head, p in flandmark[frameID].items():
-                    desc[current_st].setdefault(head, {'central':[],
-                                                       'size':[],
-                                                       'size_rap':[],
-                                                       'lip_nb_mouv_OF1':[],
-                                                       'lip_mean_mouv_x1':[],
-                                                       'lip_mean_mouv_y1':[],
-                                                       'lip_std_mouv_x1':[],
-                                                       'lip_std_mouv_y1':[],
-                                                       'lip_nb_mouv_OF2':[],
-                                                       'lip_mean_mouv_x2':[],
-                                                       'lip_mean_mouv_y2':[],
-                                                       'lip_std_mouv_x2':[],
-                                                       'lip_std_mouv_y2':[],
-                                                       'lip_mouv_mx1_p1':[],
-                                                       'lip_mouv_my1_p1':[],
-                                                       'lip_mouv_mx2_p1':[],
-                                                       'lip_mouv_my2_p1':[],
-                                                       'lip_mouv_mx1_p2':[],
-                                                       'lip_mouv_my1_p2':[],
-                                                       'lip_mouv_mx2_p2':[],
-                                                       'lip_mouv_my2_p2':[],
-                                                       'lip_histo1':[],
-                                                       'lip_histo2':[]
-                                                       })
-
-                    x, y, w, h, mx1, my1, mx2, my2 = p
-                    size = w*h
-                    l_size.append(w*h)
-                    centre_face_x = x + w/2
-
-                    if nb_face == 2: 
-                        center_window_x1 = video_w/3
-                        center_window_x2 = 2*video_w/3
-                        central = min(abs(centre_face_x-center_window_x1), abs(centre_face_x-center_window_x2))
-                    else:
-                        center_window_x = video_w/2   
-                        central = abs(centre_face_x-center_window_x)
-
-                    desc[current_st][head]['central'].append(central/video_w)
-                    desc[current_st][head]['size'].append(w*h/video_h)
-
-                for head, p in flandmark[frameID].items():
-                    x, y, w, h, mx1, my1, mx2, my2 = p
-                    desc[current_st][head]['size_rap'].append(1 - (w*h / max(l_size)))
-
-                for head in flandmark[frameID]:
-                    x, y, w, h, mx1, my1, mx2, my2 = flandmark[frameID][head]
-
-                    if head in flandmark[frameID-1]:
-                        x_p1, y_p1, w_p1, h_p1, mx1_p1, my1_p1, mx2_p1, my2_p1 = flandmark[frameID-1][head]
-                        desc[current_st][head]['lip_mouv_mx1_p1'].append(abs(mx1-mx1_p1))
-                        desc[current_st][head]['lip_mouv_my1_p1'].append(abs(my1-my1_p1))
-                        desc[current_st][head]['lip_mouv_mx2_p1'].append(abs(mx2-mx2_p1))
-                        desc[current_st][head]['lip_mouv_my2_p1'].append(abs(my2-my2_p1))
-
-                    if head in flandmark[frameID-2]:
-                        x_p2, y_p2, w_p2, h_p2, mx1_p2, my1_p2, mx2_p2, my2_p2 = flandmark[frameID-2][head]
-                        desc[current_st][head]['lip_mouv_mx1_p2'].append(abs(mx1-mx1_p2))
-                        desc[current_st][head]['lip_mouv_my1_p2'].append(abs(my1-my1_p2))
-                        desc[current_st][head]['lip_mouv_mx2_p2'].append(abs(mx2-mx2_p2))
-                        desc[current_st][head]['lip_mouv_my2_p2'].append(abs(my2-my2_p2))
-
-                    # features based on OF
-                    centre_mouth_x = (mx2+mx1)/2
-                    centre_mouth_y = (my2+my1)/2
-                    h_mouth = (mx2-mx1)/3
-
-                    score1, l_move_x1, l_move_y1 = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous1[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
-                    desc[current_st][head]['lip_nb_mouv_OF1'].append(len(l_move_x1))
-                    if l_move_x1 != []:
-                        desc[current_st][head]['lip_mean_mouv_x1'].append(np.mean(np.array(l_move_x1)))
-                        desc[current_st][head]['lip_std_mouv_x1'].append(np.std(np.array(l_move_x1)))
-                    if l_move_y1 != []:                        
-                        desc[current_st][head]['lip_mean_mouv_y1'].append(np.mean(np.array(l_move_y1)))
-                        desc[current_st][head]['lip_std_mouv_y1'].append(np.std(np.array(l_move_y1)))
-
-                    score1, l_move_x2, l_move_y2 = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous2[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
-                    desc[current_st][head]['lip_nb_mouv_OF2'].append(len(l_move_x2))
-                    if l_move_x2 != []:
-                        desc[current_st][head]['lip_mean_mouv_x2'].append(np.mean(np.array(l_move_x2)))
-                        desc[current_st][head]['lip_std_mouv_x2'].append(np.std(np.array(l_move_x2)))
-                    if l_move_y2 != []:
-                        desc[current_st][head]['lip_mean_mouv_y2'].append(np.mean(np.array(l_move_y2)))
-                        desc[current_st][head]['lip_std_mouv_y2'].append(np.std(np.array(l_move_y2)))
-                    
-                    # features based on histogram diff
-                    m_x_center = (mx1+mx2)/2                        
-                    m_y_center = (my1+my2)/2
-                    mw = (mx2-mx1)/6
-                    mh = (mx2-mx1)/8
-
-                    if mw != 0.0 and mh != 0.0:
-                        hist_current = calcul_hist(frame_current[m_y_center-mh:m_y_center+mh, m_x_center-mw:m_x_center+mw])
-                        score, l_move_x, l_move_y = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous1[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
-                        move_x =0.0
-                        move_y =0.0
-                        if l_move_x != []:
-                            move_x = int(round(np.mean(l_move_x),0))
-                            move_y = int(round(np.mean(l_move_y),0))
-
-                        ROIymin, ROIymax, ROIxmin, ROIxmax = norm_roi(m_y_center-mh+move_y, m_y_center+mh+move_y, m_x_center-mw+move_x, m_x_center+mw+move_x, video_w, video_h)
-                        hist_previous1 = calcul_hist(frame_previous1[ROIymin:ROIymax, ROIxmin:ROIxmax])   
-                        score, l_move_x, l_move_y = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous2[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
-                        move_x =0.0
-                        move_y =0.0
-                        if l_move_x != []:
-                            move_x = int(round(np.mean(l_move_x),0))
-                            move_y = int(round(np.mean(l_move_y),0))
-
-                        ROIymin, ROIymax, ROIxmin, ROIxmax = norm_roi(m_y_center-mh+move_y, m_y_center+mh+move_y, m_x_center-mw+move_x, m_x_center+mw+move_x, video_w, video_h)
-                        hist_previous2 = calcul_hist(frame_previous2[ROIymin:ROIymax, ROIxmin:ROIxmax])   
-                        desc[current_st][head]['lip_histo1'].append(cv.CompareHist(hist_current, hist_previous1, cv.CV_COMP_CORREL))
-                        desc[current_st][head]['lip_histo2'].append(cv.CompareHist(hist_current, hist_previous2, cv.CV_COMP_CORREL))
-    return desc  
-
-if __name__ == "__main__": 
-    # read arguments   
-    args = docopt(__doc__)
-    fout = open(args['<output_desc>'], 'w')
-
-    flandmark = read_flandmark_file(args['<flandmark>'])
-    st_seg = []
-    for line in open(args['<st_seg>']).read().splitlines():
-        v, p, start, dur, spk, na, na, st = line.split(' ')
-        st_seg.append([float(start), float(start)+float(dur), st])
-    
-    frame2time = IDXHack(args['<idx_file>'])
-
-    desc = compute_features(flandmark, args['<video>'], frame2time, st_seg)
-
-    for st in desc:
-        for head in sorted(desc[st]):
-            fout.write(str(st)+' '+str(head))
-            fout.write(' '+str(len(desc[st][head])))
-            for descID in sorted(desc[st][head]):
-                if desc[st][head][descID] == []:
-                    fout.write(' -1 -1')
+                centre_face_x = x + w/2
+                if nb_face == 2: 
+                    center_window_x1 = video_w/3
+                    center_window_x2 = 2*video_w/3
+                    central = min(abs(centre_face_x-center_window_x1), abs(centre_face_x-center_window_x2))
                 else:
-                    fout.write(' '+str(np.mean(desc[st][head][descID]))+' '+str(np.std(desc[st][head][descID])))
-            fout.write('\n')
+                    center_window_x = video_w/2   
+                    central = abs(centre_face_x-center_window_x)
+
+                fout.write(" "+str(central/video_w))
+                fout.write(" "+str(w*h/video_h))
+                fout.write(" "+str(1 - (w*h / max(l_size))))
+
+                if head in flandmark[frameID-1]:
+                    x_p1, y_p1, w_p1, h_p1, mx1_p1, my1_p1, mx2_p1, my2_p1 = flandmark[frameID-1][head]
+                    fout.write(" "+str(abs(mx1-mx1_p1)))
+                    fout.write(" "+str(abs(my1-my1_p1)))
+                    fout.write(" "+str(abs(mx2-mx2_p1)))
+                    fout.write(" "+str(abs(my2-my2_p1))) 
+                else:
+                    fout.write(" -1 -1 -1 -1")
+
+                if head in flandmark[frameID-2]:
+                    x_p2, y_p2, w_p2, h_p2, mx1_p2, my1_p2, mx2_p2, my2_p2 = flandmark[frameID-2][head]
+                    fout.write(" "+str(abs(mx1-mx1_p2)))
+                    fout.write(" "+str(abs(my1-my1_p2)))
+                    fout.write(" "+str(abs(mx2-mx2_p2)))
+                    fout.write(" "+str(abs(my2-my2_p2))) 
+                else:
+                    fout.write(" -1 -1 -1 -1")
+
+                # features based on OF
+                centre_mouth_x = (mx2+mx1)/2
+                centre_mouth_y = (my2+my1)/2
+                h_mouth = (mx2-mx1)/3
+
+                score1, l_move_x1, l_move_y1 = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous1[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
+                fout.write(" "+str(len(l_move_x1)))
+                if l_move_x1 != []:
+                    fout.write(" "+str(np.mean(np.array(l_move_x1))))
+                    fout.write(" "+str(np.std(np.array(l_move_x1))))
+                    fout.write(" "+str(np.mean(np.array(l_move_y1))))
+                    fout.write(" "+str(np.std(np.array(l_move_y1))))
+                else:
+                    fout.write(" -1 -1 -1 -1")
+
+                score2, l_move_x2, l_move_y2 = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous2[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
+                fout.write(" "+str(len(l_move_x2)))
+                if l_move_x2 != []:
+                    fout.write(" "+str(np.mean(np.array(l_move_x2))))
+                    fout.write(" "+str(np.std(np.array(l_move_x2))))
+                    fout.write(" "+str(np.mean(np.array(l_move_y2))))
+                    fout.write(" "+str(np.std(np.array(l_move_y2))))
+                else:
+                    fout.write(" -1 -1 -1 -1")
+                
+                # features based on histogram diff
+                m_x_center = (mx1+mx2)/2                        
+                m_y_center = (my1+my2)/2
+                mw = (mx2-mx1)/6
+                mh = (mx2-mx1)/8
+
+                if mw != 0.0 and mh != 0.0:
+                    hist_current = calcul_hist(frame_current[m_y_center-mh:m_y_center+mh, m_x_center-mw:m_x_center+mw])
+                    score, l_move_x, l_move_y = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous1[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
+                    move_x =0.0
+                    move_y =0.0
+                    if l_move_x != []:
+                        move_x = int(round(np.mean(l_move_x),0))
+                        move_y = int(round(np.mean(l_move_y),0))
+
+                    ROIymin, ROIymax, ROIxmin, ROIxmax = norm_roi(m_y_center-mh+move_y, m_y_center+mh+move_y, m_x_center-mw+move_x, m_x_center+mw+move_x, video_w, video_h)
+                    hist_previous1 = calcul_hist(frame_previous1[ROIymin:ROIymax, ROIxmin:ROIxmax])   
+                    score, l_move_x, l_move_y = OpticalFlow(frame_current[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], frame_previous2[centre_mouth_y-h_mouth:centre_mouth_y+h_mouth, mx1:mx2], lk_params, feature_params)
+                    move_x =0.0
+                    move_y =0.0
+                    if l_move_x != []:
+                        move_x = int(round(np.mean(l_move_x),0))
+                        move_y = int(round(np.mean(l_move_y),0))
+
+                    ROIymin, ROIymax, ROIxmin, ROIxmax = norm_roi(m_y_center-mh+move_y, m_y_center+mh+move_y, m_x_center-mw+move_x, m_x_center+mw+move_x, video_w, video_h)
+                    hist_previous2 = calcul_hist(frame_previous2[ROIymin:ROIymax, ROIxmin:ROIxmax])  
+
+                    fout.write(" "+str(cv.CompareHist(hist_current, hist_previous1, cv.CV_COMP_CORREL)))
+                    fout.write(" "+str(cv.CompareHist(hist_current, hist_previous2, cv.CV_COMP_CORREL)))
+                else:
+                    fout.write(" -1 -1")
+                fout.write('\n')
     fout.close()
