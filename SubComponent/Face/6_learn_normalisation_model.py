a9a9dddc9e469876c4f828ca1aff372d400fe43c
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-28 17:25:00 +0200

    initial add

---
+++ SubComponent/Face/6_learn_normalisation_model.py
@@ -0,0 +1,44 @@
+"""
+Learn a normalisation model to compute svs matrix
+
+Usage:
+  6_learn_normalisation_model.py <video_list> <facetrack_pos> <matrix_path> <reference_head_position_path> <output_model_file>
+  6_learn_normalisation_model.py -h | --help
+"""
+
+from docopt import docopt
+from mediaeval_util.repere import align_facetrack_ref
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    X = []
+    Y = []
+    for videoID in open(args['<video_list>']).read().splitlines():
+        print videoID
+
+        facetracks = {}
+        for line in open(args['<facetrack_pos>']+'/'+videoID+'.facetrack').read().splitlines():
+            frameID, faceID, xmin, ymin, w, h = map(int, line.split(' ')) 
+            facetracks.setdefault(frameID, {})
+            facetracks[frameID][faceID] = xmin, ymin, xmin+w, ymin+h
+        ref_f = read_ref_facetrack_position(args['<reference_head_position_path>']+'/'+videoID+'.position', tempo_margin)
+        facetrack_vs_ref = align_facetrack_ref(ref_f, facetracks)
+
+        for line in open(args['<matrix_path>']+'/'+videoID+'.mat').read().splitlines():
+            ft1, ft2, dist = line.split(' ')
+            if ft1 in facetrack_vs_ref and ft2 in track_to_name:
+                X.append([float(dist)])
+                if track_to_name[ft1] == track_to_name[ft2]:
+                    Y.append(1)
+                else:
+                    Y.append(0)
+
+    # train model
+    clf = CalibratedClassifierCV(LogisticRegression(), method='sigmoid')
+    clf.fit(X, Y) 
+    # save model
+    joblib.dump(clf, args['<output_model_file>']) 
---
+++ SubComponent/Face/7_normalisation_matrix.py
@@ -0,0 +1,23 @@
+"""
+Compute speaker versus speaker distance
+
+Usage:
+  normalisation_matrix.py <videoID> <input_seg> <input_mat> <model_file> <output_mat>
+  normalisation_matrix.py -h | --help
+"""
+
+from docopt import docopt
+from sklearn.externals import joblib
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    # open model
+    clas = joblib.load(args['<model_file>']) 
+
+    # compute score between speech turn and save it
+    fout = open(args['<output_mat>'], 'w')
+    for line in open(args['<input_mat>']).read().splitlines():
+        ft1, ft2, dist = line.split(' ')
+        fout.write(ft1+' '+ft2+' '+str(clas.predict_proba([[float(dist)]])[0][1])+'\n')
+    fout.close()
