a35f08fd738c7a77036af89cb1c4efa3775982e4
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-28 11:52:38 +0200

    use scipy for clustering

--- SubComponent/SpeechTurn/8_diarization.py
+++ SubComponent/SpeechTurn/8_diarization.py
@@ -5,85 +5,43 @@ Usage:
   8_diarization.py <videoID> <st_seg> <matrix> <output_diarization> [--threshold=<t>]
   8_diarization.py -h | --help
 Options:
-  --threshold=<t>  stop criterion of the agglomerative clustering [default: 0.27]
+  --threshold=<t>  stop criterion of the agglomerative clustering [default: 0.73]
 """
 
 from docopt import docopt
 from pyannote.parser import MDTMParser
 import numpy as np
-import copy
-
-
-def diarization(mat, original_mat, threshold):
-    # agglomerative clustering
-    max_proba = np.inf
-    while(max_proba>threshold):
-        max_proba = -np.inf
-        best_c1, best_c2 = '', ''
-        for c1 in mat:
-            for c2 in mat[c1]:
-                if mat[c1][c2] > max_proba:
-                    max_proba = mat[c1][c2]
-                    best_c1 = c1
-                    best_c2 = c2
-
-        if max_proba>threshold:
-            new_c = best_c1+';'+best_c2
-            # remove best_c1 best_c2 from mat
-            del mat[best_c1]
-            del mat[best_c2]
-            for clus in mat:
-                if best_c1 in mat[clus]:
-                    del mat[clus][best_c1]
-                if best_c2 in mat[clus]:
-                    del mat[clus][best_c2]
-
-            # add new_c in mat
-            mat[new_c] = {}
-            for c in mat:      
-                if c != new_c:  
-                    moyenne = 0.0
-                    count_d = 0.0        
-                    for st_new_c in new_c.split(';'):
-                        for st_c in c.split(';'): 
-                            moyenne += original_mat[st_c][st_new_c]
-                            count_d+=1.0
-                    mat[c][new_c] = moyenne/count_d  
-                    mat[new_c][c] = moyenne/count_d
-    return mat
+from scipy import spatial, cluster
 
 if __name__ == '__main__':
     args = docopt(__doc__)
 
-    threshold = float(args['--threshold'])
+    label_to_indice = {}
+    seg_st = MDTMParser().read(args['<st_seg>'])(uri=args['<videoID>'], modality="speaker")
+    for s, t, l in seg_st.itertracks(label=True):
+        label_to_indice[l] = t
 
     # read matrix
-    mat = {}
+    N = len(label_to_indice)
+    X = np.zeros((N, N))
     for line in open(args['<matrix>']).read().splitlines():
         st1, st2, proba = line.split(' ')  
-        proba = float(proba)
-        mat.setdefault(st1, {})
-        mat[st1][st2] = proba
-        mat.setdefault(st2, {})
-        mat[st2][st1] = proba
-    original_mat = copy.deepcopy(mat)
+        proba = -float(proba)
+        X[label_to_indice[st1]][label_to_indice[st2]] = proba
+        X[label_to_indice[st2]][label_to_indice[st1]] = proba
 
-    mat = diarization(mat, original_mat, threshold)
+    y = spatial.distance.squareform(X, checks=False)
+    Z = cluster.hierarchy.average(y)
+    clusters = cluster.hierarchy.fcluster(Z, float(args['--threshold']), criterion='distance')
 
-    st_to_clusters = {}
-    for c1 in mat:
-        for t1 in c1.split(';'):
-            st_to_clusters[t1] = c1
-        for c2 in mat[c1]:
-            for t2 in c2.split(';'):
-                st_to_clusters[t2] = c2
-
-    seg_st = MDTMParser().read(args['<st_seg>'])(uri=args['<videoID>'], modality="speaker")
+    st_to_clus = {}
+    for c, st in zip(clusters, label_to_indice):
+        st_to_clus[st] = c
 
     fout = open(args['<output_diarization>'], 'w')
     for s, t, l in seg_st.itertracks(label=True):
-        fout.write(args['<videoID>']+' 1 '+str(s.start)+' '+str(s.duration)+' speaker na na '+st_to_clusters[l]+'\n')
+        fout.write(args['<videoID>']+' 1 '+str(s.start)+' '+str(s.duration)+' speaker na na spk_'+str(st_to_clus[l])+'\n')
     fout.close()
-
+    
 
 
