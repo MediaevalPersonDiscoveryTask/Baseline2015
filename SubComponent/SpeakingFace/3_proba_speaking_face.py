3de0530d6c755f040583a06bdcf34e2b8a1cedb6
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-28 23:33:13 +0200

    to be tested

--- SubComponent/SpeakingFace/3_proba_speaking_face.py
+++ SubComponent/SpeakingFace/3_proba_speaking_face.py
@@ -2,20 +2,44 @@
 compute probability that a facetrack is speaking
 
 Usage:
-  proba_speaking_face.py <descriptors> <input_model_file> <output_mat>
+  proba_speaking_face.py <descriptors> <st_seg> <idx> <input_model_file> <output_mat>
   proba_speaking_face.py -h | --help
 """
 
 from docopt import docopt
 from sklearn.externals import joblib
+import numpy as np
 
 if __name__ == '__main__':
     args = docopt(__doc__)
 
     clf = joblib.load(args['<input_model_file>']) 
 
-    fout = open(args['<output_mat>'], 'w')
+    frame2time = IDXHack(args['<idx>'])
+
+    st_seg = []
+    for line in open(args['<st_seg>']).read().splitlines():
+        v, p, start, dur, spk, na, na, st = line.split(' ')
+        st_seg.append([float(start), float(start)+float(dur), st])
+
+    proba_st = {}
     for line in open(args['<descriptors>']).read().splitlines():
         l = line.split(' ')
-        fout.write(l[0]+' '+l[1]+' '+str(clf.predict_proba([map(float, l[2:])])[0][1])+'\n')
+        frameID = int(l[0])
+        timestamp = frame2time(frameID, 0.0)
+        faceID = int(l[1])
+        proba = clf.predict_proba([map(float, l[2:])])[0][1])
+        for startTime, endTime, st in st_seg:
+            if startTime <= timestamp and timestamp <= endTime:
+                break
+        proba_st.setdefault(st, {})
+        proba_st[st].setdefault(faceID, []).append(proba)
+
+
+    fout = open(args['<output_mat>'], 'w')
+    for st in proba_st:
+        for faceID, l_proba in proba_st[st].items:
+            proba = np.median(np.array(l_proba))
+            fout.write(st+' '+faceID+' '+str(proba)+'\n')
     fout.close()
+    
