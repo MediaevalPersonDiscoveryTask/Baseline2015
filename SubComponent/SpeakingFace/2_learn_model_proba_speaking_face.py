61caf63537d9431943d4cb0aa6ec11cd3479f811
johann poignant <poignant@h-0-0.local>
2015-04-29 17:53:16 +0200

    it works

--- SubComponent/SpeakingFace/2_learn_model_proba_speaking_face.py
+++ SubComponent/SpeakingFace/2_learn_model_proba_speaking_face.py
@@ -10,64 +10,79 @@ from docopt import docopt
 import numpy as np
 from mediaeval_util.repere import IDXHack, read_ref_facetrack_position, align_facetrack_ref, align_st_ref
 from sklearn.calibration import CalibratedClassifierCV
-from sklearn.linear_model import Perceptron, LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.externals import joblib
-import math
-
+import random
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-
-    tempo_margin = 3
 
     idxPath = {}
     for path in open(args['<dataPath.lst>']).read().splitlines():
         videoID, wave_file, video_avi_file, video_mpeg_file, trs_file, xgtf_file, idx_file = path.split(' ')
         idxPath[videoID] = args['<corpusPath>']+'/'+idx_file
 
-    X = []
-    Y = []
+    X, Y = [], []
     for videoID in open(args['<video_list>']).read().splitlines():
         print videoID
 
-        frame2time = IDXHack(idxPath[videoID])
-
+        # find the name corresponding to facetracks
+        ref_f = read_ref_facetrack_position(args['<reference_head_path>']+'/'+videoID+'.position', 3)
         facetracks = {}
+        l_facetrack_used_to_learn_model = set([])
         for line in open(args['<facetrack_pos>']+'/'+videoID+'.facetrack').read().splitlines():
             frameID, faceID, xmin, ymin, w, h = map(int, line.split(' ')) 
             facetracks.setdefault(frameID, {})
             facetracks[frameID][faceID] = xmin, ymin, xmin+w, ymin+h
-        ref_f = read_ref_facetrack_position(args['<reference_head_path>']+'/'+videoID+'.position', tempo_margin)
-        
-        ref_spk = []
-        for line in open(args['<reference_speaker_path>']+'/'+videoID+'.atseg').read().splitlines():
-            v, startTime, endTime, spkName = line.split(' ') 
-            ref_spk.append([float(startTime), float(endTime), spkName])
-        ref_spk.sort()
-
+            if frameID in ref_f:
+                l_facetrack_used_to_learn_model.add(faceID)
         facetrack_vs_ref = align_facetrack_ref(ref_f, facetracks)
 
-        for line in open(args['<descriptor_path>']+'/'+videoID+'.desc').read().splitlines():
-            l = line.split(' ')
-            frameID = int(l[0])
-            timestamp = frame2time(frameID, 0.0)
+        # read speaker reference
+        ref_spk = {}
+        for line in open(args['<reference_speaker_path>']+'/'+videoID+'.atseg').read().splitlines():
+            v, startTime, endTime, spkName = line.split(' ') 
+            ref_spk[spkName] = [float(startTime), float(endTime)]
+
+        # fct to convert frameID to timestamp 
+        frame2time = IDXHack(idxPath[videoID])
+
+        # read visual descriptors
+        visual_desc = {}
+        for line in open(args['<descriptor_path>']+'/'+videoID+'.desc'):
+            l = line[:-1].split(' ')
             faceID = int(l[1])
-            desc = map(float, l[2:])
+            if faceID in l_facetrack_used_to_learn_model:
+                timestamp = frame2time(int(l[0]), 0.0)
+                for spk, time in ref_spk.items():
+                    if timestamp >= time[0] and timestamp <= time[1]:
+                        visual_desc.setdefault(spk, {})
+                        visual_desc[spk].setdefault(faceID, []).append(map(float, l[2:]))
 
-            for startTime, endTime, spkName in ref_spk:
-                if startTime <= timestamp and timestamp <= endTime:
-                    break
+        # compute a descriptor with information of speaker segment
+        for spk in visual_desc:
+            startTime, endTime = ref_spk[spk]
+            spk_duration = (endTime-startTime)*25
+            l = []
+            for faceID in visual_desc[spk]:
+                l.append(float(len(visual_desc[spk][faceID])))
+            best_facetrack_cooc_duration = max(l)
 
-            SpeakingFace = 0
-            if faceID in facetrack_vs_ref:
-                if facetrack_vs_ref[faceID] == spkName:
-                    SpeakingFace = 1
+            for faceID in visual_desc[spk]:
+                facetrack_cooc_duration = float(len(visual_desc[spk][faceID]))
+                propdur_best_duration_faceID = facetrack_cooc_duration/best_facetrack_cooc_duration
+                prop_dur_spk_dur = facetrack_cooc_duration/spk_duration
 
-            X.append(desc)
-            Y.append(SpeakingFace)
+                # find if the face correspond to the speaker
+                SpeakingFace = 0
+                if faceID in facetrack_vs_ref:
+                    if facetrack_vs_ref[faceID] == spk:
+                        SpeakingFace = 1
 
-    clf = CalibratedClassifierCV(Perceptron(), method='sigmoid')
-    # train model
+                for desc in visual_desc[spk][faceID]:
+                    X.append([propdur_best_duration_faceID, prop_dur_spk_dur]+desc)
+                    Y.append(SpeakingFace)
+
+    clf = CalibratedClassifierCV(LogisticRegression(), method='sigmoid')
     clf.fit(X, Y) 
-    # save model
     joblib.dump(clf, args['<output_model_file>']) 
--- SubComponent/SpeakingFace/3_proba_speaking_face.py
+++ SubComponent/SpeakingFace/3_proba_speaking_face.py
@@ -14,35 +14,54 @@ import numpy as np
 if __name__ == '__main__':
     args = docopt(__doc__)
 
+    # load classifier model
     clf = joblib.load(args['<input_model_file>']) 
 
-    frame2time = IDXHack(args['<idx>'])
-
+    # read speech turn segmentation
     st_seg = []
     for line in open(args['<st_seg>']).read().splitlines():
         v, p, start, dur, spk, na, na, st = line.split(' ')
-        st_seg.append([float(start), float(start)+float(dur), st])
+        st_seg[st] = [float(start), float(start)+float(dur)]
 
-    proba_st = {}
-    for line in open(args['<descriptors>']).read().splitlines():
-        l = line.split(' ')
-        frameID = int(l[0])
-        timestamp = frame2time(frameID, 0.0)
+    # fct to convert frameID to timestamp 
+    frame2time = IDXHack(args['<idx>'])
+
+    # read visual descriptors
+    visual_desc = {}
+    for line in open(args['<descriptors>']):
+        l = line[:-1].split(' ')
         faceID = int(l[1])
-        desc = map(float, l[2:])
-        proba = clf.predict_proba([desc])[0]
-        print faceID, timestamp, proba
-        for startTime, endTime, st in st_seg:
-            if startTime <= timestamp and timestamp <= endTime:
-                break
-        proba_st.setdefault(st, {})
-        proba_st[st].setdefault(faceID, []).append(proba)
+        timestamp = frame2time(int(l[0]), 0.0)
+        for st, time in st_seg.items():
+            if timestamp >= time[0] and timestamp <= time[1]:
+                visual_desc.setdefault(st, {})
+                visual_desc[st].setdefault(faceID, []).append(map(float, l[2:]))
 
+    # compute a descriptor with information of speaker segment and compute the corresponding probability that the face is speaking
+    proba_st_facetrack = {}
+    for st in visual_desc:
+        proba_st_facetrack[st] = {}
 
+        startTime, endTime = seg_st[st]
+        spk_duration = (endTime-startTime)*25
+        l = []
+        for faceID in visual_desc[st]:
+            l.append(float(len(visual_desc[st][faceID])))
+        best_facetrack_cooc_duration = max(l)
+
+        for faceID in visual_desc[st]:
+            proba_st_facetrack[st][faceID] = []
+            duration = float(len(visual_desc[st][faceID]))
+            propdur_best_duration_faceID = duration/best_facetrack_cooc_duration
+            prop_dur_spk_dur = duration/spk_duration
+            for desc in visual_desc[st][faceID]:
+                proba_st_facetrack[st][faceID].append(clf.predict_proba([[propdur_best_duration_faceID, prop_dur_spk_dur]+desc])[0][1])
+
+    # save matrix
     fout = open(args['<output_mat>'], 'w')
-    for st in proba_st:
-        for faceID, l_proba in proba_st[st].items:
-            proba = np.median(np.array(l_proba))
-            fout.write(st+' '+faceID+' '+str(proba)+'\n')
+    for st in proba_st_facetrack:
+        for faceID in proba_st_facetrack[st]:
+            proba = np.median(np.array(proba_st_facetrack[st][faceID]))            
+            fout.write(st+' '+str(faceID)+' '+str(proba)+'\n')
     fout.close()
-    
+
