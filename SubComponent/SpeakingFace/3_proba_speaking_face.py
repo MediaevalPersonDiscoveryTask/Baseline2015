b45ee0b8227425b2b2ba9d3a935b9ae3545ad4ac
Johann Poignant <Johann.poignant@limsi.fr>
2015-04-29 08:32:32 +0200

    correction calcul proba + print

--- SubComponent/SpeakingFace/3_proba_speaking_face.py
+++ SubComponent/SpeakingFace/3_proba_speaking_face.py
@@ -28,7 +28,8 @@ if __name__ == '__main__':
         frameID = int(l[0])
         timestamp = frame2time(frameID, 0.0)
         faceID = int(l[1])
-        proba = clf.predict_proba([map(float, l[2:])])[0][1])
+        proba = clf.predict_proba([map(float, l[2:])])[0][1]
+        print faceID, timestamp, proba
         for startTime, endTime, st in st_seg:
             if startTime <= timestamp and timestamp <= endTime:
                 break
