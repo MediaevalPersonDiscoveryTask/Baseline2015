523651a436e50fdd08413cbdf7866f78578b6d7b
johann poignant <poignant@h-0-35.local>
2015-04-29 21:43:38 +0200

    add recall speaking face

--- SubComponent/Evaluation/face_tracking.py
+++ SubComponent/Evaluation/face_tracking.py
@@ -1,11 +1,11 @@
 """
 Usage:
-  face_tracking.py <video_list> <facetrack_pos> <reference_head_position_path>
+  face_tracking.py <video_list> <facetrack_pos> <reference_head_position_path> <idx_path> <speaker_ref>
   face_tracking.py -h | --help
 """
 
 from docopt import docopt
-from mediaeval_util.repere import read_ref_facetrack_position, align_facetrack_ref
+from mediaeval_util.repere import IDXHack, read_ref_facetrack_position, align_facetrack_ref
 
 if __name__ == '__main__':
     args = docopt(__doc__)
@@ -14,19 +14,28 @@ if __name__ == '__main__':
     nb_ref = 0.0
     nb_correct=0.0
 
+    nb_ref_speakingFace = 0.0
+    nb_correct_speakingFace = 0.0
+
     for videoID in open(args['<video_list>']).read().splitlines():
-        print videoID
 
         ref_f = read_ref_facetrack_position(args['<reference_head_position_path>']+'/'+videoID+'.position', 0)
+
+        ref_spk = []
+        for line in open(args['<speaker_ref>']+videoID+'.atseg').read().splitlines():
+            v, startTime, endTime, spkName = line.split(' ') 
+            ref_spk.append([float(startTime), float(endTime), spkName])
+
+        frame2time = IDXHack(args['<idx_path>']+videoID+'.MPG.idx')
 
         facetracks = {}
         l_ft = []
         for line in open(args['<facetrack_pos>']+'/'+videoID+'.facetrack').read().splitlines():
             frameID, faceID, xmin, ymin, w, h = map(int, line.split(' ')) 
+            facetracks.setdefault(frameID, {})
+            facetracks[frameID][faceID] = xmin, ymin, xmin+w, ymin+h
             if frameID in ref_f:
                 l_ft.append(faceID)
-                facetracks.setdefault(frameID, {})
-                facetracks[frameID][faceID] = xmin, ymin, xmin+w, ymin+h
 
         facetrack_vs_ref = align_facetrack_ref(ref_f, facetracks)
 
@@ -34,8 +43,23 @@ if __name__ == '__main__':
         for frameID in ref_f:
             nb_ref+=len(ref_f[frameID])
 
+        for frameID in ref_f:
+
+            timestamp =  frame2time(frameID, 0.0)
+            for startTime, endTime, spkName in ref_spk:
+                if timestamp >= startTime and timestamp <= endTime:
+                    for headName in ref_f[frameID]:
+                        if headName == spkName:
+                            nb_ref_speakingFace+=1
+                    if frameID in facetracks:
+                        for faceID in facetracks[frameID]:
+                            if faceID in facetrack_vs_ref and facetrack_vs_ref[faceID] == spkName:
+                                nb_correct_speakingFace+=1
+
         nb_correct+=len(facetrack_vs_ref)
 
-    print 'precision:', round(nb_correct/nb_hyp,3)*100, '%   recall:', round(nb_correct/nb_ref,3)*100, '%'
+    print 'precision:', round(nb_correct/nb_hyp,3)*100, '%    ',
+    print 'recall:',    round(nb_correct/nb_ref,3)*100, '%    ',
+    print 'recall speaking face:',    round(nb_correct_speakingFace/nb_ref_speakingFace,3)*100, '%    '
 
 
