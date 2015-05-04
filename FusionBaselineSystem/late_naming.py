53e226716739e91e1936c4f7ba03a39afa0356b5
Johann Poignant <johann.poignant@imag.fr>
2015-05-04 18:39:10 +0200

    correction import

--- FusionBaselineSystem/late_naming.py
+++ FusionBaselineSystem/late_naming.py
@@ -12,7 +12,7 @@ from docopt import docopt
 from pyannote.parser import MDTMParser, REPEREParser
 from pyannote.algorithms.tagging import HungarianTagger, ConservativeDirectTagger
 from pyannote.core import Annotation, Segment
-from mediaeval_util.repere import parser_vtseg, parser_shot_seg
+from mediaeval_util.repere import parser_vtseg, ShotSegParser
 
 
 if __name__ == "__main__":   
