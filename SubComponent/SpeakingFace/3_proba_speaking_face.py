b3a64624ccde471cecc76e8bbd6e4c0b4428a433
Johann Poignant <johann.poignant@imag.fr>
2015-05-08 01:15:34 +0200

    c

--- SubComponent/SpeakingFace/3_proba_speaking_face.py
+++ SubComponent/SpeakingFace/3_proba_speaking_face.py
@@ -2,7 +2,7 @@
 compute probability that a facetrack is speaking
 
 Usage:
-  proba_speaking_face.py <videoID> <SpeakingFaceDescriptor> <speechTurnSegmentation> <idx> <modelProbaSpeakingFace> <probaSpeakingFace>
+  proba_speaking_face.py <videoID> <facetrack> <SpeakingFaceDescriptor> <speechTurnSegmentation> <idx> <modelProbaSpeakingFace> <probaSpeakingFace>
   proba_speaking_face.py -h | --help
 """
 
@@ -19,12 +19,10 @@ if __name__ == '__main__':
 
     # read speech turn segmentation
     st_seg, confs, timeToFrameID = MESegParser(args['<speechTurnSegmentation>'], args['<videoID>'])
-
-    st_seg = {}
-    for line in open(args['<speechTurnSegmentation>']).read().splitlines():
-        v, p, start, dur, spk, na, na, st = line.split(' ')
-        st_seg[st] = [float(start), float(start)+float(dur)]
-
+    facetrack, confs, timeToFrameID = MESegParser(args['<facetrack>'], args['<videoID>'])
+    faceID_to_TrackID = {}
+    for s, trackID, faceID in facetrack.itertracks(label=True):
+        faceID_to_TrackID[faceID] = trackID
     # fct to convert frameID to timestamp 
     frame2time = IDXHack(args['<idx>'])
 
@@ -32,34 +30,35 @@ if __name__ == '__main__':
     visual_desc = {}
     for line in open(args['<SpeakingFaceDescriptor>']):
         l = line[:-1].split(' ')
-        faceID = int(l[1])
+        trackID_face = faceID_to_TrackID[int(l[1])]
         timestamp = frame2time(int(l[0]), 0.0)        
-        for s, t, st in st_seg.itertracks(label=True):
+        for s, trackID_st, st in st_seg.itertracks(label=True):
             if timestamp >= s.start and timestamp <= s.end:
-                visual_desc.setdefault(st, {})
-                visual_desc[st].setdefault(faceID, []).append(map(float, l[2:]))
+                visual_desc.setdefault(trackID_st, {})
+                visual_desc[trackID_st].setdefault(trackID_face, []).append(map(float, l[2:]))
 
     # compute a descriptor with information of speaker segment and compute the corresponding probability that the face is speaking
     proba_st_facetrack = {}
-    for s, t, st in st_seg.itertracks(label=True):
-        if st in visual_desc:
-            proba_st_facetrack[st] = {}
+    for s, trackID_st, st in st_seg.itertracks(label=True):
+        if trackID_st in visual_desc:
+            proba_st_facetrack[trackID_st] = {}
             l = []
-            for faceID in visual_desc[st]: l.append(float(len(visual_desc[st][faceID])))
+            for trackID_face in visual_desc[trackID_st]: 
+                l.append(float(len(visual_desc[trackID_st][trackID_face])))
             best_facetrack_cooc_duration = max(l)
-
-            for faceID in visual_desc[st]:
-                proba_st_facetrack[st][faceID] = []
-                duration = float(len(visual_desc[st][faceID]))
+            for trackID_face in visual_desc[trackID_st]:
+                proba_st_facetrack[trackID_st][trackID_face] = []
+                duration = float(len(visual_desc[trackID_st][trackID_face]))
                 propdur_best_duration_faceID = duration/best_facetrack_cooc_duration
                 prop_dur_spk_dur = duration/(s.duration*25)
-                for desc in visual_desc[st][faceID]: proba_st_facetrack[st][faceID].append(clf.predict_proba([[propdur_best_duration_faceID, prop_dur_spk_dur]+desc])[0][1])
+                for desc in visual_desc[trackID_st][trackID_face]: 
+                    proba_st_facetrack[trackID_st][trackID_face].append(clf.predict_proba([[propdur_best_duration_faceID, prop_dur_spk_dur]+desc])[0][1])
 
     # save matrix
     fout = open(args['<probaSpeakingFace>'], 'w')
-    for st in proba_st_facetrack:
-        for faceID in proba_st_facetrack[st]:
-            proba = np.median(np.array(proba_st_facetrack[st][faceID]))            
-            fout.write(st+' '+str(faceID)+' '+str(proba)+'\n')
+    for trackID_st in sorted(proba_st_facetrack):
+        for trackID_face in sorted(proba_st_facetrack[trackID_st]):
+            proba = np.median(np.array(proba_st_facetrack[trackID_st][trackID_face]))            
+            fout.write(str(trackID_st)+' '+str(trackID_face)+' '+str(proba)+'\n')
     fout.close()
 
