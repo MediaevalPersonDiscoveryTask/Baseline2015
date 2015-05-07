180aa2662ba1d978d52a390787cbe1ea1c34dc05
Johann Poignant <johann.poignant@imag.fr>
2015-05-08 00:36:24 +0200

    used MESeg

--- SubComponent/SpeakingFace/3_proba_speaking_face.py
+++ SubComponent/SpeakingFace/3_proba_speaking_face.py
@@ -2,7 +2,7 @@
 compute probability that a facetrack is speaking
 
 Usage:
-  proba_speaking_face.py <SpeakingFaceDescriptor> <speechTurnSegmentation> <idx> <modelProbaSpeakingFace> <probaSpeakingFace>
+  proba_speaking_face.py <videoID> <SpeakingFaceDescriptor> <speechTurnSegmentation> <idx> <modelProbaSpeakingFace> <probaSpeakingFace>
   proba_speaking_face.py -h | --help
 """
 
@@ -18,6 +18,8 @@ if __name__ == '__main__':
     clf = joblib.load(args['<modelProbaSpeakingFace>']) 
 
     # read speech turn segmentation
+    st_seg, confs, timeToFrameID = MESegParser(args['<speechTurnSegmentation>'], args['<videoID>'])
+
     st_seg = {}
     for line in open(args['<speechTurnSegmentation>']).read().splitlines():
         v, p, start, dur, spk, na, na, st = line.split(' ')
@@ -31,31 +33,27 @@ if __name__ == '__main__':
     for line in open(args['<SpeakingFaceDescriptor>']):
         l = line[:-1].split(' ')
         faceID = int(l[1])
-        timestamp = frame2time(int(l[0]), 0.0)
-        for st, time in st_seg.items():
-            if timestamp >= time[0] and timestamp <= time[1]:
+        timestamp = frame2time(int(l[0]), 0.0)        
+        for s, t, st in st_seg.itertracks(label=True):
+            if timestamp >= s.start and timestamp <= s.end:
                 visual_desc.setdefault(st, {})
                 visual_desc[st].setdefault(faceID, []).append(map(float, l[2:]))
 
     # compute a descriptor with information of speaker segment and compute the corresponding probability that the face is speaking
     proba_st_facetrack = {}
-    for st in visual_desc:
-        proba_st_facetrack[st] = {}
+    for s, t, st in st_seg.itertracks(label=True):
+        if st in visual_desc:
+            proba_st_facetrack[st] = {}
+            l = []
+            for faceID in visual_desc[st]: l.append(float(len(visual_desc[st][faceID])))
+            best_facetrack_cooc_duration = max(l)
 
-        startTime, endTime = st_seg[st]
-        spk_duration = (endTime-startTime)*25
-        l = []
-        for faceID in visual_desc[st]:
-            l.append(float(len(visual_desc[st][faceID])))
-        best_facetrack_cooc_duration = max(l)
-
-        for faceID in visual_desc[st]:
-            proba_st_facetrack[st][faceID] = []
-            duration = float(len(visual_desc[st][faceID]))
-            propdur_best_duration_faceID = duration/best_facetrack_cooc_duration
-            prop_dur_spk_dur = duration/spk_duration
-            for desc in visual_desc[st][faceID]:
-                proba_st_facetrack[st][faceID].append(clf.predict_proba([[propdur_best_duration_faceID, prop_dur_spk_dur]+desc])[0][1])
+            for faceID in visual_desc[st]:
+                proba_st_facetrack[st][faceID] = []
+                duration = float(len(visual_desc[st][faceID]))
+                propdur_best_duration_faceID = duration/best_facetrack_cooc_duration
+                prop_dur_spk_dur = duration/(s.duration*25)
+                for desc in visual_desc[st][faceID]: proba_st_facetrack[st][faceID].append(clf.predict_proba([[propdur_best_duration_faceID, prop_dur_spk_dur]+desc])[0][1])
 
     # save matrix
     fout = open(args['<probaSpeakingFace>'], 'w')
