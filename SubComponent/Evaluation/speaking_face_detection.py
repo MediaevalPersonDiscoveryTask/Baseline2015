dff6dfb837a272abc7d032e48cac9b41d4cefc65
Johann Poignant <johann.poignant@imag.fr>
2015-05-08 02:10:42 +0200

    c

--- SubComponent/Evaluation/speaking_face_detection.py
+++ SubComponent/Evaluation/speaking_face_detection.py
@@ -67,6 +67,7 @@ if __name__ == '__main__':
         speaking_frame = {}
         for line in open(args['<matrix_speaking_face>']+videoID+'.mat').read().splitlines():
             trackID_st, trackID_face, proba = line.split(' ')
+            print trackID_face_to_faceID[int(trackID_face)]
             if trackID_face_to_faceID[int(trackID_face)] in l_ft:
                 proba = float(proba)
                 speaking_frame.setdefault(trackID_st_to_st[int(trackID_st)], [proba, trackID_face_to_faceID[int(trackID_face)]])
